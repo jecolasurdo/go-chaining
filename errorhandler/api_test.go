98c847c1dc7f88a96a0c168c4d342f438d06b39d
Joe <joecolasurdo@eltoro.com>
2017-07-25 16:33:13 -0700

    Added basic test covererage.

--- errorhandler/api_test.go
+++ errorhandler/api_test.go
@@ -1,6 +1,7 @@
 package errorhandler_test
 
 import (
+	"errors"
 	"testing"
 
 	"jecolasurdo/go-deferrederrors/errorhandler"
@@ -10,7 +11,99 @@ import (
 
 func Test_TryBool_ActionTrueNoError_ReturnsTrue(t *testing.T) {
 	d := new(errorhandler.DeferredErrorContext)
-	action := func() (bool, error) { return true, nil }
-	result := d.TryBool(action)
+	trueAction := func() (bool, error) { return true, nil }
+	result := d.TryBool(trueAction)
 	assert.True(t, result)
+}
+
+func Test_TryBool_ActionFalseNoError_ReturnsFalse(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	falseAction := func() (bool, error) { return false, nil }
+	result := d.TryBool(falseAction)
+	assert.False(t, result)
+}
+
+func Test_TryBool_PreviousError_IgnoresAction(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	timesActionWasCalled := 0
+	action := func() (bool, error) {
+		timesActionWasCalled++
+		return true, nil
+	}
+
+	d.LocalError = errors.New("test error")
+	d.TryBool(action)
+
+	assert.Equal(t, 0, timesActionWasCalled)
+}
+
+func Test_TryBool_PreviousError_ReturnsFalse(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	trueAction := func() (bool, error) {
+		return true, nil
+	}
+
+	d.LocalError = errors.New("test error")
+	result := d.TryBool(trueAction)
+
+	assert.False(t, result)
+}
+
+func Test_TryBool_ActionErrors_SetsLocalError(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	errorAction := func() (bool, error) { return false, errors.New("test error") }
+
+	d.TryBool(errorAction)
+
+	assert.NotNil(t, d.LocalError)
+}
+
+func Test_TryVoid_NoPreviousError_ExecutesAction(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	timesActionWasCalled := 0
+	action := func() error {
+		timesActionWasCalled++
+		return nil
+	}
+
+	d.TryVoid(action)
+
+	assert.Equal(t, 1, timesActionWasCalled)
+}
+
+func Test_TryVoid_PreviousError_IgnoresAction(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	timesActionWasCalled := 0
+	action := func() error {
+		timesActionWasCalled++
+		return nil
+	}
+
+	d.LocalError = errors.New("test error")
+	d.TryVoid(action)
+
+	assert.Equal(t, 0, timesActionWasCalled)
+}
+
+func Test_TryVoid_ActionErrors_SetsLocalError(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	errorAction := func() error { return errors.New("test error") }
+
+	d.TryVoid(errorAction)
+
+	assert.NotNil(t, d.LocalError)
+}
+
+func Test_FlushError_Normally_ResetsErrorToNil(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	d.LocalError = errors.New("test error")
+	d.FlushError()
+	assert.Nil(t, d.LocalError)
+}
+
+func Test_FlushError_Normally_ReturnsAnyEror(t *testing.T) {
+	d := new(errorhandler.DeferredErrorContext)
+	d.LocalError = errors.New("test error")
+	err := d.FlushError()
+	assert.NotNil(t, err)
 }
