c0329e56074a7348a2eabbf102be0ac9fbba5104
Joe <joecolasurdo@eltoro.com>
2017-07-27 17:50:24 -0700

    Unimplemented methods return not implemented error.

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package chaining
 
 import "jecolasurdo/go-chaining/injectionbehavior"
+import "errors"
 
 // Flush returns the context's error and final result, and resets the context back to its default state.
 func (c *Context) Flush() (interface{}, error) {
@@ -32,7 +33,10 @@ func (c *Context) ApplyNullary(action func() error) {
 //
 // The error returned by the supplied action is also applied to the current context.
 // If error is not nil, subsequent actions executed within the same context will be ignored.
-func (c *Context) ApplyNullaryIface(action func() (interface{}, error)) {}
+func (c *Context) ApplyNullaryIface(action func() (interface{}, error)) {
+	c.LocalError = errors.New("ApplyNullaryIface not implemented")
+	c.PreviousActionResult = nil
+}
 
 // ApplyUnary executes an action which takes one argument returns only an error.
 //
@@ -44,7 +48,10 @@ func (c *Context) ApplyNullaryIface(action func() (interface{}, error)) {}
 //
 // The error returned by the supplied action is also applied to the current context.
 // If error is not nil, subsequent actions executed within the same context will be ignored.
-func (c *Context) ApplyUnary(action func(interface{}) error, arg ActionArg) {}
+func (c *Context) ApplyUnary(action func(interface{}) error, arg ActionArg) {
+	c.LocalError = errors.New("ApplyUnary not implemented")
+	c.PreviousActionResult = nil
+}
 
 // ApplyUnaryIface executes an action which takes one argument, and returns a tuple of (interface{}, error).
 //
@@ -103,5 +110,7 @@ func (c *Context) ApplyNullaryBool(action func() (bool, error)) bool {
 // In addition to threading the (bool, error) tuple into the current context, UnaryBool itself also returns a bool.
 // This is useful for inlining the method in boolean statements.
 func (c *Context) ApplyUnaryBool(action func(interface{}) (bool, error), arg ActionArg) bool {
+	c.LocalError = errors.New("ApplyUnaryBool not implemented")
+	c.PreviousActionResult = nil
 	return false
 }
